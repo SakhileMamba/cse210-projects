8ba14f4d05d35e8c69e7c5fcf041e078d3992c19
Sakhile Mamba <sakhymamba@gmail.com>
2023-09-15 16:49:46 +0200

    c# prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAnotherGame = "yes";
+
+        do {
+
+        bool keepPlaying = true;
+
+        Random randomNumberGenerator = new Random();
+         int magicNumber = randomNumberGenerator.Next(1, 100);
+
+        int numberOfGuesses = 0;
+
+        while (keepPlaying)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            numberOfGuesses++;
+
+            if (guess < magicNumber)
+            {   
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                keepPlaying = false;
+            }
+
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine($"You made {numberOfGuesses} guesses.");
+
+        Console.Write("Would you like to play again? yes/no: ");
+        playAnotherGame = Console.ReadLine();
+        Console.WriteLine("");
+
+        } while (playAnotherGame == "yes");
     }
 }
