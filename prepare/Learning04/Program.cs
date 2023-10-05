ca3e1cab4620a606a7166c646fdda98134720bbd
Sakhile Mamba <sakhymamba@gmail.com>
2023-10-05 21:12:46 +0200

    W04 Learning Complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,7 +3,20 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    {   
+        Assignment assignment1 = new Assignment("Sakhile Mamba", "Some Topic");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+        
+        // Now create the derived class assignments
+        MathAssignment assignment2 = new MathAssignment("Sakhile Mamba", "Division", "10.3", "1-2");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Sakhile Mamba", "English", "The world is falling apart!");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
     }
 }
