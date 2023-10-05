ab2ebeac8e1895924c62a8a038643754dc349051
Sakhile Mamba <sakhymamba@gmail.com>
2023-10-06 01:07:45 +0200

    Designer Team Activity 04 Completed

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,25 @@
+public class ListingActivity : Activity
+{
+    protected int _count;
+    protected List<String> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return _prompts;
+    }
+}
---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity : Activity
+{
+    protected List<string> _prompts;
+    protected List<string> _questions;
+
+    public ReflectingActivity()
+    {
+        
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
