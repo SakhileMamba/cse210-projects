26354b54d2d9e85334030fed18bf062488d87f5d
Sakhile Mamba <sakhymamba@gmail.com>
2023-10-12 00:59:01 +0200

    Learning05 Complete

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius, string color) : base(color: color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(color: "red", side: 15.0);
+        Console.WriteLine($"Square color: {square.GetColor()}");
+        Console.WriteLine($"Square area: {square.GetArea()}");
+        Console.WriteLine();
+
+        Rectangle rectangle = new Rectangle(color: "blue", length: 10.0, width: 5);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        Console.WriteLine();
+
+        Circle circle = new Circle(color: "pink", radius: 50.0);
+        Console.WriteLine($"Circle color: {circle.GetColor()}");
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine();
+
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        foreach (Shape shape in shapes)
+        {   
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
+
     }
 }
